18d61d08b88859e48e386f1c64b81a1caa25aa30
Pankaj Dubey <pankaj_dubey30@yahoo.com>
2025-11-30 12:58:43 +0530

    Merged 3 commits into one

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -7,11 +7,24 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     public class Person
-    { 
+    {
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Title { get; set; }   
+        public Address PersonalAddress { get; set; }
+        public Address ProfessionalAddress { get; set; }
+
+
+
+    }
+    public class Address
+    {
+        public int Id { get; set; }
         public string City { get; set; }
         public string State { get; set; }
 
-        public string LastName { get; set; }
+
     }
-}
+} 
+
